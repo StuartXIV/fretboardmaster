6dbea6fc7530655a00372d5b36e803c8c3231483
Peter Episcopo <peteralbertstuart@hotmail.it>
2021-10-27 02:55:22 +0100

    Player Choices addition

--- app.js
+++ app.js
@@ -6,15 +6,18 @@ const header = document.querySelector("h1");
 const button = document.getElementById("button");
 const icons = document.querySelectorAll("i");
 const computer_choice = document.getElementById("computer-choice");
+const player_choice = document.getElementById("player-choice");
+const player_choices = Array.from(document.querySelectorAll(".note-choice"));
+let score = 0;
 
-const player_choices = [];
+
 
 const notes = ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#","D",/*end of 10*/  "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", /*end of E string 20*/ "A#", "B", "C", "C#","D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#","D", "D#", "E", "F", /*end of A string 20*/ "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#","D", "D#", "E", "F", "D", "D#", "E", "F", "F#", /*end of D string 20*/ "G#", "A", "A#", "B", "C", "C#","D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#","D", "D#" /*open strings*/, "A", "D", "G"];
 
 
 // const notes = ["E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1", "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5", "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6"];
 
-const letters = ["a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"];
+const letters = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
 
 let previous_note;
 
@@ -23,7 +26,7 @@ function onLoad(){
     header.style.fontSize = "7vh";
     computer_choice.innerHTML = "";
     setTimeout(() => { bass.style.transform = "translateX(0px)";}, 1000);
-    setTimeout(() => { button.style.opacity = "1"; }, 1900);    
+    setTimeout(() => { button.style.opacity = "1"; }, 1900);      
     
 }
 
@@ -51,25 +54,67 @@ function randomLetter (){
 
 function previousNumber (x){    
     previous_note = document.getElementById(x);
-    console.log(x);
     computer_choice.innerHTML = notes[x];      
     
 };
 
-document.getElementById('button').addEventListener("click", function(){   
+document.getElementById('button').addEventListener("click", function(){
+    
+    activateChoices();
+    audio.src = "./audio/f1.mp3";   
+    changeButton(); 
     previous_note.style.opacity = "0";
+    computer_choice.style.opacity = "0";
+    player_choice.innerHTML = ""; //remove previous player choice
+    player_choice.style.background = "linear-gradient(3600deg, #b96d41, #ffd587 100%)";
     let random = generateRandomTwo ();
     let note = document.getElementById(`${random}`);
     previousNumber(random);
     note.style.opacity = "1";
-    audio.src = "./audio/f1.mp3";
-    audio.play();
-
- 
-    
-
-
+    audio.play();    
 });
+
+function changeButton(){
+    if (button.innerHTML != "SKIP") {
+        button.innerHTML = "SKIP";
+        button.style.background = "orange";
+    } else {
+        button.innerHTML = "PLAY!";
+        button.style.background = "#297416ec";
+    }
+}
+
+//PLAYERS CHOICE ----------------------------------------------------------------------
+
+//Add event listeners to the player's options, match the index to the array 'letters', confirm player's choice. Change colours
+function activateChoices(){
+player_choices.forEach(item => {
+    item.addEventListener("click", function(){
+        let index_of_choice = player_choices.indexOf(item);
+        player_choice.innerHTML = letters[index_of_choice].toUpperCase();
+        computer_choice.style.opacity = "1";
+        changeColor();        
+        button.innerHTML = "PLAY!";
+        button.style.background = "#297416ec";
+    })
+})
+}
+
+function changeColor(){
+    if (player_choice.innerHTML === computer_choice.innerHTML){
+        player_choice.style.background = "#297416ec";
+        earnPoint();
+    } else {
+        player_choice.style.background = "red";
+    }
+};
+
+function earnPoint(){   ////////////////NOT WORKING!!!
+    score++;
+    console.log(score);
+}
+
+
 
 previousNumber(40);
 onLoad();
